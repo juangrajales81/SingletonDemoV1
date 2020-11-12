2e7dfda13fe440a7916135a17cae957a2fcd84b8
juangrajales81 <juangrajales81@gmail.com>
2020-11-12 14:50:31 -0500

    Add project files.

---
+++ SingletonDemoV1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromTeacher = Singleton.GetInstance;
+            fromTeacher.PrintDetails("From teacher");
+
+            Singleton fromEstudent = Singleton.GetInstance;
+            fromEstudent.PrintDetails("From student");
+
+            Console.ReadLine();
+        }
+    }
+}
